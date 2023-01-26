858080126006cd2267764df4918f8b94ceff59a2
Kayxzz <66139977+neegroo@users.noreply.github.com>
2023-01-26 18:09:02 +0100

    Add files via upload

---
+++ commands/clear.js
@@ -0,0 +1,89 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "clear",
+  description: "Borra la cola",
+  usage: "",
+  permissions: {
+    channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
+    member: [],
+  },
+  aliases: ["cl", "cls"],
+  /**
+   *
+   * @param {import("../structures/DiscordMusicBot")} client
+   * @param {import("discord.js").Message} message
+   * @param {string[]} args
+   * @param {*} param3
+   */
+  run: async (client, message, args, { GuildDB }) => {
+    let player = await client.Manager.get(message.guild.id);
+    if (!player)
+      return client.sendTime(
+        message.channel,
+        "❌ | **No hay nada sonando ahora mismo...**"
+      );
+
+    if (!player.queue || !player.queue.length || player.queue.length === 0)
+      return client.sendTime(
+        message.channel,
+        "❌ | **No hay nada sonando ahora mismo...**"
+      );
+    if (!message.member.voice.channel)
+      return client.sendTime(
+        message.channel,
+        "❌ | **Necesitas estar en un canal de voz para poder reproducir algo!**"
+      );
+    if (
+      message.guild.me.voice.channel &&
+      message.member.voice.channel.id !== message.guild.me.voice.channel.id
+    )
+      return client.sendTime(
+        message.channel,
+        "❌ | **Necesitas estar en un canal de voz para poder usar este comando!**"
+      );
+    player.queue.clear();
+    await client.sendTime(message.channel, "✅ | **La cola se ha limpiado!**");
+  },
+
+  SlashCommand: {
+    /**
+     *
+     * @param {import("../structures/DiscordMusicBot")} client
+     * @param {import("discord.js").Message} message
+     * @param {string[]} args
+     * @param {*} param3
+     */
+    run: async (client, interaction, args, { GuildDB }) => {
+      const guild = client.guilds.cache.get(interaction.guild_id);
+      const member = guild.members.cache.get(interaction.member.user.id);
+      if (!member.voice.channel)
+        return client.sendTime(
+          interaction,
+          "❌ | You must be in a voice channel to use this command."
+        );
+      if (
+        guild.me.voice.channel &&
+        !guild.me.voice.channel.equals(member.voice.channel)
+      )
+        return client.sendTime(
+          interaction,
+          "❌ | **Necesitas estar en un canal de voz para poder usar este comando!**"
+        );
+      let player = await client.Manager.get(interaction.guild_id);
+      if (!player)
+        return client.sendTime(
+          interaction,
+          "❌ | **No hay nada sonando ahora mismo...**"
+        );
+
+      if (!player.queue || !player.queue.length || player.queue.length === 0)
+        return client.sendTime(
+          interaction,
+          "❌ | **No hay nada sonando ahora mismo...**"
+        );
+      player.queue.clear();
+      await client.sendTime(interaction, "✅ | **Cleared the queue!**");
+    },
+  },
+};
